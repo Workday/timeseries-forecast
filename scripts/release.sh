f490d83eda0136df85434bc119522942f5fe432f
ace.hao <ace.hao@workday.com>
2017-05-16 11:28:32 -0700

    udpate release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-mvn -B release:prepare
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    mvn -B release:prepare
+    mvn release:rollback
+    git tag -d timeseries-forecast-1.0.0
+    git push origin :refs/tags/timeseries-forecast-1.0.0
+    mvn release:clean
+fi
+
